ad9848e38b42b0f215e4924528ebb31b1e256d68
alex baldwin <ghostbaldwin@gmail.com>
2023-10-03 17:17:12 -0400

    oauth2

--- lib/types.ts
+++ lib/types.ts
@@ -21,6 +21,10 @@ export interface Options {
    */
   token_secret?: string;
   /**
+   * OAuth2 credential. Required for Oauth endpoints.
+   */
+  bearer?: string;
+  /**
    * (optional) The API url if different from the default.
    */
   baseUrl?: string;
